99b6eb46e1e9e52c78773561d7893c20e2930eec
gang.tang@cutt.com <gang.tang@cutt.com>
2017-08-15 16:45:04 +0800

    06

--- Python帮助手册/06-高阶函数.py
+++ Python帮助手册/06-高阶函数.py
@@ -44,7 +44,7 @@ l2 = filter(lambda x:not x%2,l1)
 print l2
 
 #5)sorted:
-###sorted(iterable,cmp=None,key=None,reverse=False
+###sorted(iterable,cmp=None,key=None,reverse=False)
 """
 1.cmp，比较的函数，这个具有两个参数，参数的值都是从可迭代对象中取出，此函数必须遵守的规则为，大于则返回1，小于则返回-1，等于则返回0
 2.key，主要是用来进行比较的元素，只有一个参数，具体的函数的参数就是取自于可迭代对象中，指定可迭代对象中的一个元素来进行排序
